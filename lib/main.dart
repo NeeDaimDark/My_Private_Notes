452f15963fd7e58fa29fa122f687cde926eef447
unknown <mohamedamine.koubaa@esprit.tn>
2025-07-31 15:47:18 +0100

    getting used to blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:ffi';
 
 
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:myprivatenotes/services/auth/auth_service.dart';
 import 'package:myprivatenotes/views/notes/notes_view.dart';
 import 'package:myprivatenotes/views/notes/create_update_note_view.dart';
@@ -42,7 +44,143 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+     _textController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create:(context) => CounterBloc(),
+        child : Scaffold(
+          appBar: AppBar(
+            title: const Text('Counter Bloc Example'),
+          ),
+          body : BlocConsumer<CounterBloc, CounterState>(
+            listener: (context, state){
+              _textController.clear();
+
+            },
+            builder: (context,state){
+              final invalidValue = (state is CounterStateInvalidNumber) ?
+              state.invalidValue : '';
+              return Column(
+                children: [
+                  Text('current value: ${state.value}'),
+                  Visibility(child: Text('Invalid input: $invalidValue'),
+                    visible: state is CounterStateInvalidNumber,
+                  ),
+                  TextField(controller: _textController,
+                  decoration:  const InputDecoration(
+                    hintText: 'Enter a number',
+                  ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: (){
+                          context.read<CounterBloc>().add(
+                            DecrementCounterEvent(_textController.text),
+                          );
+                        },
+                        child: const Text('-'),
+                      ),
+                      TextButton(onPressed: (){
+                        context.read<CounterBloc>().add(
+                          IncrementCounterEvent(_textController.text),
+                        );
+                      },
+                          child: const Text('+'),
+                      )
+                    ],
+                  )
+                ],
+              );
+            },
+          ),
+        )
+    );
+  }
+}
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(int value) : super(value);
+}
+class CounterStateInvalidNumber extends CounterState {
+  final String invalidValue;
+
+  const CounterStateInvalidNumber({
+    required this.invalidValue,
+    required int previousValue,
+  }) : super(previousValue);
+}
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+  const CounterEvent(this.value);
+}
+
+class IncrementCounterEvent extends CounterEvent {
+  const IncrementCounterEvent(String value) : super(value);
+}
+
+class DecrementCounterEvent extends CounterEvent {
+  const DecrementCounterEvent(String value) : super(value);
+}
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterStateValid(0)) {
+    on<IncrementCounterEvent>((event, emit) {
+      final integer = int.tryParse(event.value);
+      if (integer == null) {
+        emit(CounterStateInvalidNumber(
+          invalidValue: event.value,
+          previousValue: state.value,
+        ),
+        );
+      } else {
+        emit(CounterStateValid(state.value + integer));
+      }
+    });
+    on<DecrementCounterEvent>((event, emit) {
+      final integer = int.tryParse(event.value);
+      if (integer == null) {
+        emit(CounterStateInvalidNumber(
+          invalidValue: event.value,
+          previousValue: state.value,
+        ),
+        );
+      } else {
+        emit(CounterStateValid(state.value - integer));
+      }
+    });
+  }
+}
+/*class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
@@ -72,8 +210,7 @@ class HomePage extends StatelessWidget {
 
         );
   }
-}
-
+}*/
 
 
 
